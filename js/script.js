5dbfc8c8d5c7bd10b3ee3d3d542bd98f1d16b7a4
DmitriyGurov <gurov93.gp@gmail.com>
2017-05-15 01:16:23 +0400

    дописал явускр
    анимация формы

--- js/script.js
+++ js/script.js
@@ -5,17 +5,47 @@ var popup = document.querySelector(".footer-form");
 
 var close = popup.querySelector(".form-modal-close");
 
-var login = popup.querySelector("[name=name]");
+var name = popup.querySelector("[name=name]");
+
+var email = popup.querySelector("[name=email]");
 
 var form = popup.querySelector("form");
+
+var storage = localStorage.getItem("name");
 
 link.addEventListener("click", function(event) {
 	event.preventDefault();
 	popup.classList.add("footer-form-show");
-	login.focus();
-    });	
+	if (storage) {
+		name.value = storage;
+		email.focus();
+		} else {
+		name.focus();
+		}
+	});	
 
 close.addEventListener("click", function(event) {
-    event.preventDefault();
-    popup.classList.remove("footer-form-show");
-    });
+event.preventDefault();
+	popup.classList.remove("footer-form-show");
+	popup.classList.remove("footer-form-error");
+	});
+
+form.addEventListener("submit", function(event) {
+	if (!name.value || !email.value) {
+		event.preventDefault();
+		popup.classList.remove("footer-form-error");
+		popup.offsetWidth = popup.offsetWidth;
+		popup.classList.add("footer-form-error");
+		} else {
+			localStorage.setItem("name", name.value);
+		}
+	});
+
+window.addEventListener("keydown", function(event) {
+	if (event.keyCode === 27) {
+		if (popup.classList.contains("footer-form-show")) {
+			popup.classList.remove("footer-form-show");
+			popup.classList.remove("footer-form-error");
+			}
+		}
+	});
