f5c6efa33bbdf986383c33b847ce0604a7261efb
DmitriyGurov <gurov93.gp@gmail.com>
2017-05-15 22:37:16 +0400

    карта интерактив
    по эскеп закрыть по крестику не реализовано еще

--- js/script.js
+++ js/script.js
@@ -13,6 +13,12 @@ var form = popup.querySelector("form");
 
 var storage = localStorage.getItem("name");
 
+var mapOpen = document.querySelector(".js-open-map");
+
+var mapPopup = document.querySelector(".modal-map");
+
+var mapClose = mapPopup.querySelector(".modal-map-close");
+
 link.addEventListener("click", function(event) {
 	event.preventDefault();
 	popup.classList.add("footer-form-show");
@@ -21,14 +27,14 @@ link.addEventListener("click", function(event) {
 		email.focus();
 		} else {
 		name.focus();
-		}
-	});	
+	}
+});	
 
 close.addEventListener("click", function(event) {
-event.preventDefault();
+	event.preventDefault();
 	popup.classList.remove("footer-form-show");
 	popup.classList.remove("footer-form-error");
-	});
+});
 
 form.addEventListener("submit", function(event) {
 	if (!name.value || !email.value) {
@@ -37,15 +43,31 @@ form.addEventListener("submit", function(event) {
 		popup.offsetWidth = popup.offsetWidth;
 		popup.classList.add("footer-form-error");
 		} else {
-			localStorage.setItem("name", name.value);
-		}
-	});
+		localStorage.setItem("name", name.value);
+	}
+});
 
 window.addEventListener("keydown", function(event) {
 	if (event.keyCode === 27) {
 		if (popup.classList.contains("footer-form-show")) {
 			popup.classList.remove("footer-form-show");
 			popup.classList.remove("footer-form-error");
-			}
 		}
-	});
+	}
+});
+
+mapOpen.addEventListener("click", function(event) {
+	mapPopup.classList.add("modal-map-show");
+});
+
+mapClose.addEventListener("click", function(event) {
+	mapPopup.classList.remove("modal-map-show");
+});
+
+window.addEventListener("keydown", function(event) {
+	if (event.keyCode === 27) {
+	if (mapPopup.classList.contains("modal-map-show")) {
+		mapPopup.classList.remove("modal-map-show");
+		}
+	}
+});
